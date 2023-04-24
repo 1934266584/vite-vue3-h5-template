69cf472c1e7ace594653cc8b8a14be75b8910edd
xiaojing93 <xj1934266584@gmail.com>
2023-04-24 11:05:26 +0800

    Update styleImport.ts

--- build/vite/plugin/styleImport.ts
+++ build/vite/plugin/styleImport.ts
@@ -2,10 +2,11 @@
  *  Introduces component library styles on demand.
  * https://github.com/anncwb/vite-plugin-style-import
  */
-import styleImport, { VantResolve } from 'vite-plugin-style-import'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 export function configStyleImportPlugin(isBuild: boolean) {
-	return styleImport({
-		resolves: [VantResolve()]
+	return Components({
+		resolvers: [VantResolver()]
 	})
 }
